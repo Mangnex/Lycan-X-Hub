227ca382be0b0de8e157b445c2d797a525335ab9
Mangnex <127064046+Mangnex@users.noreply.github.com>
2023-08-27 16:17:39 -0500

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,32 @@
+local placeId = game.PlaceId
+local UserInputService = game:GetService("UserInputService")
+local IsOnMobile
+local IsOnComputer
+
+if UserInputService.TouchEnabled then
+	IsOnMobile = true
+elseif UserInputService.MouseIconEnabled then
+	IsOnComputer = true
+else
+   IsOnComputer = true
+end
+
+if placeId == 5233782396 then
+    if IsOnComputer then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangnex/Lycan-X-Hub/main/Creature%20of%20Sonaria"))()
+    elseif IsOnMobile then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangnex/Lycan-X-Hub/main/Creature%20of%20Sonaria%20Mobile"))()
+    end
+ elseif placeId == 3095204897 then
+    if IsOnComputer then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangnex/Lycan-X-Hub/main/Isle%209%20Script"))()
+    elseif IsOnMobile then
+        game:service('Players').LocalPlayer:Kick('There is no isle 9 script for mobil!')
+    end
+    elseif placeId == 13842554790 then
+        if IsOnComputer then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangnex/Lycan-X-Hub/main/Creature%20of%20Sonaria%20Recode/CoS%20Computer%20Script.lua"))()
+        elseif IsOnMobile then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangnex/Lycan-X-Hub/main/Creature%20of%20Sonaria%20Recode/Cos%20Mobile%20Script.lua"))()
+        end
+ end
